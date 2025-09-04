b843065bc8f9c9b9ec84896775ec57e6d58a13c3
MohamadDimasPrayoga1010 <mohamaddimasprayoga89@gmail.com>
2025-09-04 11:26:52 +0700

    Add files via upload

---
+++ minitask2.js
@@ -0,0 +1,28 @@
+//informasi tentang diri kita
+//descruc painting, education name [0]
+
+const bio = {
+  name: "Mohamad Dimas Prayoga",
+  age: 23,
+};
+
+const extraInfo = {
+  skills: ["Web Programing", "Painting"],
+  education: [
+    {
+      name: "SMP 1 Denpasar",
+      year: 2012,
+    },
+  ],
+};
+
+const profile = {
+  ...bio,
+  ...extraInfo,
+}
+
+const {skills: [_,skill] } = extraInfo
+const {education : [{name}]} = profile
+
+console.log(skill)
+console.log(name)
