2437883a549d26fcdc69a0647d9db772ab835c24
MohamadDimasPrayoga1010 <mohamaddimasprayoga89@gmail.com>
2025-09-04 15:50:14 +0700

    Update minitask4.js

--- minitask4.js
+++ minitask4.js
@@ -1,12 +1,14 @@
+
+ 
 function fahrenheit(c){
-    let  konvert = c * (9 / 5) + 32;
+    let  konvert = c * (9 / 5) + 32 
     return konvert
 }
 
 
 
 const reamur = function(c){
-    let konvertReamur = (5/4) * c;
+    let konvertReamur = (5/4) * c
     return konvertReamur
 }
 
@@ -18,15 +20,21 @@ const kelvin = (c) =>{
 
 
 
-function output(c){
-    console.log("Hasil Konvert suhu Celcius " + c + " ke Farenheit adalah " + fahrenheit(c));
-    console.log(
-      "Hasil konvert suhu Celcius " + c + " ke Reamur adalah " + reamur(c)
-    );
-    console.log(
-      "Hasil konvert suhu Celcius " + c + " ke Kelvin adalah " + kelvin(c)
-    );
+// function output(c){
+//     console.log("Hasil Konvert suhu Celcius" + c + " ke Farenheit adalah " + fahrenheit(c));
+//     console.log(
+//       "Hasil konvert suhu Celcius " + c + " ke Reamur adalah " + reamur(c)
+//     );
+//     console.log("Hasil konvert suhu Celcius "+ c +" ke Kelvin adalah " + kelvin(c))
+// }
+
+// output(40)
+
+const hasil = (c, cb1, cb2, cb3) =>{
+console.log(`Celcius ${c} konversi  Farenheit  ${cb1(c)}`)
+console.log(`Celcius ${c} konversi  Reamur  ${cb2(c)}`)
+console.log(`Celcius ${c} konversi  Kelvin  ${cb3(c)}`)
+
 }
 
-output(40)
-
+hasil(5, fahrenheit, reamur, kelvin)
