2b280f4e3136d2657819c22910ea324b44e9cb56
MohamadDimasPrayoga1010 <mohamaddimasprayoga89@gmail.com>
2025-09-04 14:12:11 +0700

    Update minitask4.js

--- minitask4.js
+++ minitask4.js
@@ -1,5 +1,3 @@
-
- 
 function fahrenheit(c){
     let  konvert = c * (9 / 5) + 32;
     return konvert
@@ -21,9 +19,13 @@ const kelvin = (c) =>{
 
 
 function output(c){
-    console.log("Hasil Konvert suhu Celcius ke Farenheit adalah " + fahrenheit(c));
-    console.log("Hasil konvert suhu Celcius ke Reamur adalah " + reamur(c));
-    console.log("Hasil konvert suhu Celcius ke Kelvin adalah " + kelvin(c))
+    console.log("Hasil Konvert suhu Celcius " + c + " ke Farenheit adalah " + fahrenheit(c));
+    console.log(
+      "Hasil konvert suhu Celcius " + c + " ke Reamur adalah " + reamur(c)
+    );
+    console.log(
+      "Hasil konvert suhu Celcius " + c + " ke Kelvin adalah " + kelvin(c)
+    );
 }
 
 output(40)
